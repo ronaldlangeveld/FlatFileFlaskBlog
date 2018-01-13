4edda9e5a6de54ee304380c08cc312395cc98939
Luuc Verburgh <luuc.verburgh@gmail.com>
2018-01-13 21:45:54 +0100

    reads all .md files from posts directory and builds HTML

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import Markup
 from jinja2 import Template
+import markdown2
+import os
+
 app = Flask(__name__)
 
 # template_dir = os.path.abspath('templates/')
@@ -19,4 +24,13 @@ def main():
 
 @app.route("/posts")
 def blog():
-    return render_template('posts.html')
+
+	posts = []
+
+	#
+	# Loops over all posts and stores html in posts[]
+	#
+	for post in os.listdir("posts/"):
+		posts.append(markdown2.markdown_path("posts/" + post))
+
+	return render_template('posts.html', posts=posts)
